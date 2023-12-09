ce4cb72c7285f5be96d8c7e00952265d842c20b1
BOOK-OKPNPUFST0\onejo <onejourney27@gmail.com>
2023-12-09 13:00:16 +0900

    폭발 애니 추가, 애니메이터 연결

---
+++ Assets/Script/LobbyUIManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject creditPopup;
+
+    private void Start()
+    {
+        InitUI();
+    }
+
+    private void InitUI()
+    {
+        HideCreditPopup();
+    }
+
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ShowCreditPopup()
+    {
+        creditPopup.SetActive(true);
+    }
+    
+    public void HideCreditPopup()
+    {
+        creditPopup.SetActive(false);
+    }
+}
